bb6cdc4f1055912aa9ef4f57a0d47b88b8dac19f
Matthew Hefner <mathymattic@gmail.com>
2025-04-11 01:02:28 -0400

    Tweaked thumbnails and began spectrawhorl refactor

--- pages/papers_and_links/briggs.py
+++ pages/papers_and_links/briggs.py
@@ -7,7 +7,7 @@ dash.register_page(
     name="Annual estimates of global and national CO₂ emissions from fossil fuels: Tracking revisions to the United Nations energy statistics database input energy data", 
     title="Annual estimates of global and national CO₂ emissions from fossil fuels: Tracking revisions to the United Nations energy statistics database input energy data", 
     description="Published in Cambridge University Press' Environmental Data Science journal.", 
-    image="assets/webp/thumbnails/briggs.webp"
+    image=""
 )
 
 layout = html.Div()
--- pages/spectrawhorl/spectrawhorl.py
+++ pages/spectrawhorl/spectrawhorl.py
@@ -1,15 +1,118 @@
-import dash
-from dash import html
+"""
 
-# Tonetornado to be refactored here as spectrawhorl
+spectrawhorl.py
 
-dash.register_page(
+This script builds the spectrawhorl app's main page.
+
+M W Hefner, 2025
+MIT License
+
+"""
+
+from dash import register_page, html, dcc, clientside_callback, Input, Output, State
+import dash_bootstrap_components as dbc
+
+register_page(
     __name__, 
     path="/spectrawhorl", 
     name="tonetornado", 
     title="tonetornado", 
     description="tonetornado is a free and interactive web tool that circularly visualizes (and makes) sound (and useful overlays) for learning (and fun!)", 
-    image="/assets/webp/thumbnails/tonetornado.webp"
+    image="/assets/webp/thumbnails/SpectraWhorl.webp"
 )
 
-layout = html.Div()
+layout = html.Div(
+    
+    children = [
+
+        # A location object tracks the address bar url
+        dcc.Location(id='spectrawhorl_url'),
+        
+        # Help menu button
+        dbc.Stack([
+            dbc.Button(
+                dbc.Col(html.I(className="fa-solid fa-question fs-2"), align="center"),
+                id="spectrawhorl-help-button",
+                color="primary",
+                style={"borderRadius": "50%", "aspectRatio": "1 / 1"}
+            )
+        ], className="position-fixed bottom-0 start-0 m-3"),
+        
+        # help popover
+        dbc.Popover(
+            dbc.PopoverBody(html.Em("expand info modal")),
+            target="spectrawhorl-help-button",
+            trigger="hover",
+            placement="right"
+        ),
+        
+        # Help/information/welcome Modal
+        dbc.Modal(
+            [
+                dbc.ModalHeader(dbc.ModalTitle("Welcome")),
+                dbc.ModalBody(
+                    [
+                        
+                    dcc.Markdown("**SpectraWhorl**",
+                        style={
+                            "textAlign": "center", 
+                            "fontSize": "3em",
+                        }
+                    ),
+                    
+                dbc.Carousel(
+                    items=[
+                        {"key": "6", "src": "/assets/webp/images/tonetornado_still.webp", "caption" : "The Harmonic Series at C3"},
+                    ],
+                    controls=True,
+                    indicators=False,
+                    className="carousel-fade",
+                    interval=3000,
+                ),
+                    
+                    ]
+                ),
+                dbc.ModalFooter(
+
+                    dbc.Row([
+                        dbc.Button(
+                            "Close Info Modal",
+                            id="close-spectrawhorl-help-modal",
+                            className="ms-auto",
+                            n_clicks=0,
+                        )
+                    ], justify="center", style={"textAlign": "center"}, className = "gap-3")
+
+                ),
+            ],
+            id="spectrawhorl-help-modal",
+            centered=True,
+            size="lg"
+        ),
+        
+        # Viewer
+        html.Div(
+            
+            children=[
+                
+                # Fullscreen toggle
+                html.Button([html.I(className="fa-solid fa-up-right-and-down-left-from-center"),], id = "fullscreenToggle", className = "B_TRANS",
+                title = "Toggle fullscreen viewer"),
+                
+            ]
+        ),
+    ]
+)
+
+## Help modal open/close
+clientside_callback(
+    """
+    function(n_clicks, n_2, is_open) {
+        return !is_open;
+    }
+    """,
+    Output("spectrawhorl-help-modal", "is_open"),
+    [Input("spectrawhorl-help-button", "n_clicks"), Input("close-spectrawhorl-help-modal", "n_clicks")],
+    State("spectrawhorl-help-modal", "is_open"),
+    prevent_initial_call=False,
+)
